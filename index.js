814c80eced7684d58b43f5e55c070d28b5ff0a6e
Adrian Luley <amluley@comcast.net>
2018-08-09 15:34:33 -0400

    fixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express")
 const port = 3000
 const app = express()
@@ -6,5 +7,20 @@ app.use(express.static('public'))
 app.use(express.json())
 
 // Fill in your request handlers here
+
+app.get("/", (req, res) => {
+    res.render('index');
+})
+
+
+let updates = [];
+
+app.post("/updates", (req, res) => {
+    if(req.body.clientupdates.toString() != [])
+    updates.push(req.body.clientupdates);
+    let sentChanges = updates.slice(req.body.lastSeenIndex, updates.length)
+    sentChanges = sentChanges.filter(array => array.length > 0)
+    res.send({"updates": sentChanges, "last": updates.length});
+})
 
 app.listen(port)
